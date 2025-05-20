2d975ee12a8ecf1540aeacf65b9473c66b2b80b1
ChocolateKest <gustavoemartins@gmail.com>
2025-05-20 20:44:16 -0300

    commit

---
+++ src/main/java/com/mycompany/atividade2/Atividade2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade2;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author gusta
+ */
+public class Atividade2 {
+
+public class Projeto21 {
+
+    public static void main(String[] args) {
+        
+        for(int i=34; i<57;i=i+2){
+          System.out.println(i);  
+        }
+        
+    }
+}
+}
